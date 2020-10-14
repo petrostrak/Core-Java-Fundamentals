c786356c830dd3283439e719ae646624bdfbb90d
petros <pit.trak@gmail.com>
2020-10-14 16:44:39 +0300

    Arrays

---
+++ Arrays.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		int[][] arr1 = {{1, 2, 3},{4, 5, 6}};
+		String s = Arrays.deepToString(arr1);
+		System.out.println(s);
+		int[] arr2 = {1, 2, 3};
+		String s1 = Arrays.toString(arr2);
+		System.out.println(s1);
+	}
+
+}
