46bd8aefbb1c14729481f4acaf82860a0c8da166
pit_trak <45797153+petrostrak@users.noreply.github.com>
2020-10-16 12:44:43 +0300

    Added Lambda expression in Timer

--- timer/TimerTest.java
+++ timer/TimerTest.java
@@ -16,6 +16,12 @@ public class TimerTest {
 		Timer timer = new Timer(1000, listener);
 		timer.start();
 		
+//		// Timer with lambda expression
+//		Timer timer = new Timer(1000, event ->{
+//			System.out.println("At the tone, the time is " + Instant.ofEpochMilli(event.getWhen()));
+//			Toolkit.getDefaultToolkit().beep();
+//		});
+		
 		// Keep program running until user selects "OK"
 		JOptionPane.showMessageDialog(null, "Quit program?");
 		System.exit(0);
